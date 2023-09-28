533c7593771af4c599747f0296ab3ab2b228334c
kdy425 <101076270+kdy425@users.noreply.github.com>
2023-09-28 22:44:03 +0900

    Update process_viewer.py

--- process viewer & pe viewer/process_viewer.py
+++ process viewer & pe viewer/process_viewer.py
@@ -3,6 +3,7 @@ import psutil
 from tabulate import tabulate
 import pandas as pd
 
+
 def get_processes():
     procs = []
     for p in psutil.process_iter():
@@ -37,6 +38,8 @@ def get_processes():
             # 네트워크 사용량 정보를 가져옴
             #network_usage = get_network_usage(pid)
 
+
+#print할 부분
         procs.append({
             'pid': pid,
             'name': name,
@@ -53,18 +56,24 @@ def get_processes():
         })
     return procs
 
+
+
+#바이트 크기를 가장 적합한 단위로 변환하여 문자열로 반환하는 함수
 def get_size(bytes):
     for i in ['', 'K', 'M', 'G', 'T', 'P', 'E']:
         if bytes < 1024:
             return f"{bytes:.2f}{i}B"
         bytes /= 1024
         
+        
+#print 함수        
 def print_processes(ps):
     table = tabulate(ps, headers="keys", tablefmt='simple')
     return table if table else ""
 
 
-def get_network_connections(pid):   #네트워크 연결 유무
+#네트워크 연결 유무
+def get_network_connections(pid):  
     try:
         connections = psutil.net_connections(kind='all')
         pid_connections = [conn for conn in connections if conn.pid == pid]
@@ -72,8 +81,10 @@ def get_network_connections(pid):   #네트워크 연결 유무
     except Exception as e:
         print(f"Error fetching network connections: {e}")
         return []
+    
 
-def get_open_ports(pid):    #열린포트 번호
+#열린포트 번호
+def get_open_ports(pid):  
     try:
         connections = psutil.net_connections(kind='inet')
         pid_ports = [conn.laddr.port for conn in connections if conn.pid == pid]
@@ -87,6 +98,7 @@ def get_open_ports(pid):    #열린포트 번호
 
 #####################################################################################################
 #오류 있음
+#각 프로세스 마다 사용량이 안나오는 문제가 있음
 '''def get_network_usage(pid): #네트워크 사용량
     try:
         io = psutil.net_io_counters()
@@ -107,12 +119,13 @@ def get_open_ports(pid):    #열린포트 번호
 
 
 #메모장에 실행 결과 저장
+'''
 output = print_processes(get_processes())
 output_file_path = "C:\\Users\\ehdbs\\OneDrive\\바탕 화면\\네트워크 대역폭 사용량.txt"
 with open(output_file_path, "w") as output_file:
     output_file.write(output)
 
-print(f"프로그램 실행 결과가 {output_file_path} 파일에 저장되었습니다.")
+print(f"프로그램 실행 결과가 {output_file_path} 파일에 저장되었습니다.")'''
 
 
 
@@ -128,6 +141,6 @@ with open(output_file_path, "w") as output_file:
 
 
 #그냥 실행
-'''
+
 procs = get_processes()
-print_processes(procs)'''
+print_processes(procs)
