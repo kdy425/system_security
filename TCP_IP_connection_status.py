0bf6796999ea1c8de3d866a7cf5dec0913b03663
parkkyutae1121 <101890500+parkkyutae1121@users.noreply.github.com>
2023-10-06 17:56:08 +0900

    Update TCP_IP_connection_status.py

--- TCP_IP_connection_status.py
+++ TCP_IP_connection_status.py
@@ -1,7 +1,10 @@
 import psutil
 
 def get_network_connection():
+
+    #현재의 tcp, ip 연결 정보를 가져오기
     connection = psutil.net_connections(kind='inet')
+    
     for conn in connection:
         print(f"Family : {conn.family}")
         print(f"Type : {conn.type}")
