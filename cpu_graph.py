906ab24c8cd573efe9ad656a40bffd80c8887439
taewoong0626 <63411150+taewoong0626@users.noreply.github.com>
2023-10-28 21:56:15 +0900

    Update cpu_graph.py

--- cpu_graph.py
+++ cpu_graph.py
@@ -3,35 +3,37 @@ import matplotlib.pyplot as plt
 from collections import deque
 import time
 
-max_data_points = 50 9
+max_data_points = 50
 cpu_usage_data = deque(maxlen=max_data_points)
-time_data = deque(maxlen=max_data_points)  # 시간 데이터를 저장9
+time_data = deque(maxlen=max_data_points)
 
 plt.ion()
 fig, ax1 = plt.subplots()
 
-ax1.set_xlabel('시간')  # x축 시간
-ax1.set_ylabel('CPU 사용률 (%)', color='tab:blue')  # y축 CPU 사용률 (%)
+ax1.set_xlabel('시간')
+ax1.set_ylabel('CPU 사용률 (%)', color='tab:blue')
 
 def update_cpu_graph(pid):
     def inner_update():
         try:
-            process = psutil.Process(pid)  # 프로세스 정보
-            cpu_percent = process.cpu_percent()  # CPU 사용률
+            process = psutil.Process(pid)
+            cpu_percent = process.cpu_percent(interval=1)
 
             cpu_usage_data.append(cpu_percent)
             time_data.append(time.strftime('%H:%M:%S'))
 
             ax1.clear()
-            ax1.set_xlabel('시간')  # x축 설정
-            ax1.set_ylabel('CPU 사용률 (%)', color='tab:blue')  # y축 설정
+            ax1.set_xlabel('시간')
+            ax1.set_ylabel('CPU 사용률 (%)', color='tab:blue')
             ax1.plot(time_data, cpu_usage_data, color='tab:blue', label='CPU 사용률')
 
-            ax1.set_ylim(0, 100)  # CPU 사용률 % 표시
+            # y 축 범위를 자동으로 조정
+            ax1.relim()
+            ax1.autoscale_view()
 
             fig.tight_layout()
             plt.xticks(rotation=45)
-            plt.draw()  # 그래프 생성
+            plt.draw()
             plt.pause(0.01)
         except psutil.NoSuchProcess:
             print(f"PID {pid}에 해당하는 프로세스를 찾을 수 없습니다.")
@@ -40,12 +42,10 @@ def update_cpu_graph(pid):
     return inner_update
 
 # 프로세스의 PID
-pid = 7044
-
+pid = 26752
 
 update_func = update_cpu_graph(pid)
 
-
 while True:
     update_func()
-    time.sleep(3)
+    time.sleep(2)
