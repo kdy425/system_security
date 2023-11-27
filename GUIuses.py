9eec8e8071c34d4c5180718ba25c9355ecb37f7a
taewoong0626 <63411150+taewoong0626@users.noreply.github.com>
2023-11-27 23:33:56 +0900

    최종수정본

--- GUIuses.py
+++ GUIuses.py
@@ -7,26 +7,35 @@ class ProcessMonitorApp:
     def __init__(self, master):
         self.master = master
         self.master.title("프로세스 모니터")
-        self.master.geometry("600x500")  # 창크기
+        self.master.geometry("600x500")  # 창 크기
 
         self.create_widgets()
         self.current_procs = []  # 현재 모니터링 중인 프로세스 PID
-        self.timer_ids = []  # 타이머 식별자
+        self.timer_id = None  # 타이머 식별자
         self.previous_memory_usage = {}  # 이전 메모리 사용량
         self.new_pids = []  # 입력받은 새로운 프로세스 PID
+        self.monitoring_active = False  # 모니터링 활성화 여부 플래그
 
     def create_widgets(self):
         # GUI 위젯 생성
         ttk.Label(self.master, text="Enter the PIDs of the processes to monitor (comma-separated):").pack(pady=10)
         self.pid_entry = ttk.Entry(self.master)
         self.pid_entry.pack(pady=10)
-        ttk.Button(self.master, text="Start Monitoring", command=self.start_monitoring).pack(pady=10)
+        self.start_button = ttk.Button(self.master, text="Start Monitoring", command=self.toggle_monitoring)
+        self.start_button.pack(pady=10)
+        self.stop_button = ttk.Button(self.master, text="Stop Monitoring", command=self.stop_monitoring, state=tk.DISABLED)
+        self.stop_button.pack(pady=10)
         self.log_text = tk.Text(self.master, height=30, width=60)
         self.log_text.pack(pady=10)
 
+    def toggle_monitoring(self):
+        if self.monitoring_active:
+            self.stop_monitoring()
+        else:
+            self.start_monitoring()
+
     def start_monitoring(self):
         try:
-
             self.new_pids = [int(pid.strip()) for pid in self.pid_entry.get().split(',')]
             configure_logging()
 
@@ -34,12 +43,17 @@ class ProcessMonitorApp:
             if self.current_procs:
                 self.stop_monitoring()
 
-            #구분선
+            # 구분선
             separator_line = "\n----------------------------------------\n"
             self.log_text.insert(tk.END, separator_line)
             self.log_text.yview(tk.END)
 
             # 사용량 출력 시작
+            self.monitoring_active = True
+            self.start_button["text"] = "Pause Monitoring"
+            self.stop_button["state"] = tk.NORMAL
+
+            # 1초마다 정보 출력 타이머 시작
             self.print_one_second_usage()
 
         except ValueError:
@@ -50,13 +64,24 @@ class ProcessMonitorApp:
         for pid in self.new_pids:
             self.monitor_process(pid)
 
-
         separator_line = "\n----------------------------------------\n"
         self.log_text.insert(tk.END, separator_line)
         self.log_text.yview(tk.END)
 
         # 다음 1초 동안 정보 불러오기
-        self.master.after(1000, self.print_one_second_usage)
+        if self.monitoring_active:
+            self.timer_id = self.master.after(1000, self.print_one_second_usage)
+
+    def stop_monitoring(self):
+        # 현재 실행 중인 타이머 중지 후 상태 초기화
+        if self.timer_id is not None:
+            self.master.after_cancel(self.timer_id)
+
+        self.current_procs = []
+        self.previous_memory_usage = {}
+        self.monitoring_active = False
+        self.start_button["text"] = "Start Monitoring"
+        self.stop_button["state"] = tk.DISABLED
 
     def monitor_process(self, pid):
         try:
@@ -88,15 +113,6 @@ class ProcessMonitorApp:
             logging.error(f"PID가 {pid}인 프로세스를 찾을 수 없습니다: {e}")
         except Exception as e:
             logging.error(f"오류가 발생했습니다: {e}")
-
-    def stop_monitoring(self):
-        # 현재 실행 중인 타이머 중지 후 상태 초기화
-        for timer_id in self.timer_ids:
-            self.master.after_cancel(timer_id)
-
-        self.current_procs = []
-        self.timer_ids = []
-        self.previous_memory_usage = {}
 
 def configure_logging():
     # 로깅 정보 초기화
