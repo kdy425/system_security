8bf9524bd812931aa6248337cc01bdf9f8987ef9
parkkyutae1121 <kyutae1121@korea.ac.kr>
2023-11-29 19:13:13 +0900

    Update disk_I_O.py

--- disk_I_O.py
+++ disk_I_O.py
@@ -1,11 +1,14 @@
 import psutil
 import time
 
+def convert_bytes_to_kilobytes(bytes_value):
+    return bytes_value / 1024
+
 def monitor_disk_io(interval=1):
     while True:
         disk_io = psutil.disk_io_counters()
-        print(f"디스크 읽기 (bytes): {disk_io.read_bytes}")
-        print(f"디스크 쓰기 (bytes): {disk_io.write_bytes}")
+        print(f"디스크 읽기 (KB): {convert_bytes_to_kilobytes(disk_io.read_bytes)}")
+        print(f"디스크 쓰기 (KB): {convert_bytes_to_kilobytes(disk_io.write_bytes)}")
         print(f"디스크 읽기 (count): {disk_io.read_count}")
         print(f"디스크 쓰기 (count): {disk_io.write_count}")
         print(f"디스크 읽기 시간 (ms): {disk_io.read_time}")
